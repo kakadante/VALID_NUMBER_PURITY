14dbc2c71152113bf35aa479889791c72947dbeb
danielmasila760655 <danielmasila76@outlook.com>
2019-05-23 22:38:51 +0300

    Add project files.

---
+++ VALID NUMBER-PURITY/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VALID_NUMBER_PURITY
+{
+    /*
+     * Write a program and ask the user to enter a number. The number should be between 1 to 10.
+     * If the user enters a valid number, display "Valid" on the console. Otherwise, display "Invalid".
+     * (This logic is used a lot in applications where values entered into input boxes need to be validated.)
+     */
+
+    public class ValidNumber
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER A NUMBER: ");
+            int number = int.Parse(Console.ReadLine());
+
+
+            if (number > 0 && number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+        }
+    }
+}
